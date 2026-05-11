307deb2becbb5aafdf43188e7969a59a2fa5d11e
Brooke Kuhlmann <brooke@alchemists.io>
2026-05-11 08:33:05 -0600

    Updated TRMNL API provider to use local HTTP and logger dependencies
    With the release of TRMNL API 0.15.0, it's now possible to override the TRMNL API HTTP and logger dependencies. This leverages the new feature by ensuring the Terminus dependencies are used so all HTTP behavior and log output is consistent across the board. 🚀
    
    Milestone: minor

--- config/providers/trmnl_api.rb
+++ config/providers/trmnl_api.rb
@@ -4,6 +4,11 @@ Hanami.app.register_provider :trmnl_api do
   prepare { require "trmnl/api" }
 
   start do
+    slice.start :http
+
+    TRMNL::API::Container.register :http, slice[:http]
+    TRMNL::API::Container.register :logger, slice[:logger]
+
     recipes = TRMNL::API.new { |settings| settings.uri = "https://trmnl.com" }
 
     register :trmnl_api, TRMNL::API.new
