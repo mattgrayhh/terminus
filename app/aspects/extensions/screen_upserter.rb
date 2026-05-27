f7c6f9a8d482b03e72d9389695ba9602f97f1506
Brooke Kuhlmann <brooke@alchemists.io>
2026-05-27 09:57:45 -0600

    Updated the screen upserter to supply screen variables to the view
    Ties everything together by using the contextualizer to acquire the screen variables and then pass them to the view so the `style` element in the layout has the necessary variables to override the link:https://trmnl.com/framework[TRMNL Framework] defaults.
    
    Milestone: minor

--- app/aspects/extensions/screen_upserter.rb
+++ app/aspects/extensions/screen_upserter.rb
@@ -6,20 +6,29 @@ module Terminus
       # Creates or updates associated screen from Liquid content.
       class ScreenUpserter
         include Deps[
+          "aspects.extensions.contextualizer",
           "aspects.extensions.renderer",
           "aspects.screens.upserter",
           view: "views.extensions.dynamic"
         ]
 
         def call extension, model_id: nil, device_id: nil
+          screen_variables = load_screen_variables(extension, model_id:, device_id:)
+
           renderer.call(extension, model_id:, device_id:)
-                  .fmap { view.call content: it }
+                  .fmap { view.call screen_variables:, content: it }
                   .bind do |content|
                     upserter.call model_id:,
                                   device_id:,
                                   content: String.new(content),
                                   **extension.screen_attributes
                   end
+        end
+
+        private
+
+        def load_screen_variables extension, model_id:, device_id:
+          contextualizer.call(extension, model_id:, device_id:).fetch "screen_variables"
         end
       end
     end
