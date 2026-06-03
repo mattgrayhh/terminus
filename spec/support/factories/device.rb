0ff923eb1cdd68280d3dbab4a0147cc4b4ccd5a3
Brooke Kuhlmann <brooke@alchemists.io>
2026-06-03 10:54:42 -0600

    Updated device factory defaults
    Necessary to provide safe defaults, which match the database defaults, based on recent database migrations. Attributes are alphabetically sorted now.
    
    Milestone: minor

--- spec/support/factories/device.rb
+++ spec/support/factories/device.rb
@@ -3,13 +3,16 @@
 Factory.define :device, relation: :device do |factory|
   factory.association :model
 
-  factory.friendly_id "ABC123"
-  factory.label "Test"
   factory.api_key "abc123"
-  factory.mac_address "A1:B2:C3:D4:E5:F6"
   factory.battery_charge 0
   factory.battery_voltage 3.0
-  factory.wifi(-44)
-  factory.firmware_version "1.2.3"
+  factory.display_compatibility false
+  factory.display_profile "default"
   factory.firmware_update true
+  factory.firmware_version "1.2.3"
+  factory.friendly_id "ABC123"
+  factory.label "Test"
+  factory.mac_address "A1:B2:C3:D4:E5:F6"
+  factory.touch_bar "tap"
+  factory.wifi(-44)
 end
