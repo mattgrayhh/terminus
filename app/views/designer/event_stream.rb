30eb5a3a9eaa1f35390c74588a7e92dfde84cde7
Brooke Kuhlmann <brooke@alchemists.io>
2026-06-15 10:31:40 -0600

    Added designer event stream view
    Provides a specialized view for rendering event stream content. Will soon be used replace the rendering of raw HTML in `Terminus::Aspects::Screens::Designer::EventStream`.
    
    Issue: 321
    Milestone: minor

---
+++ app/views/designer/event_stream.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Views
+    module Designer
+      # The even stream view.
+      class EventStream < View
+        expose :screen
+      end
+    end
+  end
+end
