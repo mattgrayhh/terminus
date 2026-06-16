1dbeca12cf38c49a6d112a2c57bb3dab65154d59
Brooke Kuhlmann <brooke@alchemists.io>
2026-06-16 09:21:57 -0600

    Added screen template relation
    Necessary to provide low level database access.
    
    Issue: 321
    Milestone: minor

---
+++ app/relations/screen_template.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Relations
+    # The screen template relation.
+    class ScreenTemplate < DB::Relation
+      schema :screen_template, infer: true do
+        associations do
+          belongs_to :model, relation: :model
+          has_many :screens, relation: :screen, as: :screens
+        end
+      end
+    end
+  end
+end
